6cf7a11b83ed4d2e14a3ee0939026cbc0e1da6ca
Chen <andrewchen349@gmail.com>
2020-02-19 02:30:04 -0500

    Added Upper Nav Bar

--- gaoportfolio/src/App.js
+++ gaoportfolio/src/App.js
@@ -2,25 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//Import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+//Other Imports
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/NavBar';
+import Nav from 'react-bootstrap/Nav';
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            title: 'Irena Gao',
+            headerLinks:
+            [
+                { title: 'work', path: "/"},
+                { title: 'me',   path: "/me"},
+                { title: 'resume', path: "/resume"}
+            ],
+            home:{
+                title: "Hi I'm Irena",
+                subTitle: "I am a product designer, marketer, and front-end developer, creating technologies for communities",
+                text: "Recently, I interned at Dynalec Corp. Check out some of my work below!"
+            },
+            resume:{
+                title: "resume",
+            },
+            me:{
+                title: "About Me",
+            }
+        }
+    }
+    render(){
+       return(
+           <Router>
+               <Container fluid={true}>
+                   <Navbar bg="transparent" expand="lg">
+                       <Navbar.Toggle aria-controls="navbar-toggle"/>
+                       <Navbar.Collapse id="navbar-toggle">
+                           <Nav className="ml-auto">
+                               <h5> <Link className="nav-link display-5 font-weight-semibold" to="/">work</Link></h5>
+                               <h5> <Link className="nav-link display-5 font-weight-semibold" to="/me">me</Link></h5>
+                               <h5> <Link className="nav-link display-5 font-weight-semibold" to="/me"><span style={{color:'#fed25f'}}>resume</span></Link></h5>
+                           </Nav>
+                       </Navbar.Collapse>
+                   </Navbar>
+               </Container>
+           </Router>
+       );
+   }
+}
 export default App;
